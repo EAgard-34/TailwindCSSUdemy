9e7a29e82c06e1c7fdd9517081a057c4940bd0e8
Eric <111783532+EAgard-34@users.noreply.github.com>
2024-01-12 14:30:21 -0400

    Initial Project
    Setup new project

---
+++ my-project/src/App.jsx
@@ -0,0 +1,14 @@
+import { MdOutlineShoppingCart } from "react-icons/md"; 
+import './index.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <MdOutlineShoppingCart className="text-black lg:text-red-500" size={30}/>
+    </>
+  )
+}
+
+export default App
